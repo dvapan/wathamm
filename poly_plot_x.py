f3a173fe27dcbe517a233fe54ce9a0a42897d5fc
Danil Apanovich (MESI) <dvapan@gmail.com>
2022-01-19 16:50:58 +0800

    draw more than one p and v

--- poly_plot_x.py
+++ poly_plot_x.py
@@ -19,20 +19,20 @@ from model import eq1_left, eq1_right
 from model import eq2_left, eq2_right
 from model import make_id, psize
 
-filename = sys.argv[1]
+def get_pv(pc, in_pts):
+    ids = in_pts // np.array([ltreg,lxreg])
+    pids = np.apply_along_axis(lambda x: int(make_id(*x)),1,ids)
+    cf = pc[pids]
+    p_cf,v_cf = np.hsplit(cf, 2)
+    p = mvmonos(in_pts, ppwrs, [0, 0])
+    up = np.sum(p*p_cf, axis=1)
+    uv = np.sum(p*v_cf, axis=1)
+    return up,uv
+
+filenames = sys.argv[1:]
 
 pprx = 7
 pprt = 7
-
-pc = np.loadtxt(filename)
-print("Polynom approximate with: {}".format(pc[-1]))
-pc = pc[:-1]
-
-pc = pc.reshape(-1,psize*2)
-
-ppwrs = powers(max_poly_degree, 2)
-psize = len(ppwrs)
-
 totalx = xreg*pprx - xreg + 1
 totalt = treg*pprt - treg + 1
 X = np.linspace(0, length, totalx)
@@ -42,28 +42,35 @@ T_part = list(mit.windowed(T,n=pprt,step=pprt - 1))
 lxreg = X_part[0][-1] - X_part[0][0]
 ltreg = T_part[0][-1] - T_part[0][0]
 
-print(ltreg, lxreg)
+pcs = []
+for filename in filenames:
+    pc = np.loadtxt(filename)
+    print("Polynom approximate with: {}".format(pc[-1]))
+    pc = pc[:-1]
+    pc = pc.reshape(-1,psize*2)
+    pcs.append(pc)
+
+ppwrs = powers(max_poly_degree, 2)
+psize = len(ppwrs)
 
 cff_cnt = [psize,psize]
 
 X = np.array([0])
 T = np.arange(0, total_time, 0.01)
 
-tt,xx = np.meshgrid(T,X)
-in_pts = np.vstack([tt.flatten(),xx.flatten()]).T
-ids = in_pts // np.array([ltreg,lxreg])
-pids = np.apply_along_axis(lambda x: int(make_id(*x)),1,ids)
-
-cf = pc[pids]
-p_cf,v_cf = np.hsplit(cf, 2)
-p = mvmonos(in_pts, ppwrs, [0, 0])
-up = np.sum(p*p_cf, axis=1)
-uv = np.sum(p*v_cf, axis=1)
 fig, axs = plt.subplots(2)
 plt.subplots_adjust(left=0.1, bottom=0.25)
 
-l1, = axs[0].plot(T, up, lw=2, color='blue')
-l2, = axs[1].plot(T, uv, lw=2, color='blue')
+tt,xx = np.meshgrid(T,X)
+in_pts = np.vstack([tt.flatten(),xx.flatten()]).T
+lp = []
+lv = []
+for pc in pcs:
+    up,uv = get_pv(pc,in_pts)
+    l1, = axs[0].plot(T, up, lw=2)
+    l2, = axs[1].plot(T, uv, lw=2)
+    lp.append(l1)
+    lv.append(l2)
 axs[0].axis([0, total_time, p0*0.8, p0*1.5])
 axs[1].axis([0, total_time, -2*v0, 2*v0])
 
@@ -78,16 +85,10 @@ def update(val):
 
     tt,xx = np.meshgrid(T,X)
     in_pts = np.vstack([tt.flatten(),xx.flatten()]).T
-    ids = in_pts // np.array([ltreg,lxreg])
-    pids = np.apply_along_axis(lambda x: int(make_id(*x)),1,ids)
-    cf = pc[pids]
-    p_cf,v_cf = np.hsplit(cf, 2)
-    p = mvmonos(in_pts, ppwrs, [0, 0])
-    up = np.sum(p*p_cf, axis=1)
-    uv = np.sum(p*v_cf, axis=1)
-    
-    l1.set_ydata(up)
-    l2.set_ydata(uv)
+    for i,pc in enumerate(pcs):
+        up,uv = get_pv(pc,in_pts) 
+        lp[i].set_ydata(up)
+        lv[i].set_ydata(uv)
     fig.canvas.draw_idle()
 spos.on_changed(update)
 
